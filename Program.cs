dd5bca812833676a3c38f9f70a58cc55a6cb877d
Milton Österberg <miltonwendel@outlook.com>
2023-10-04 12:23:49 +0200

    Utryckning

--- Program.cs
+++ Program.cs
@@ -13,14 +13,14 @@ class Personal
         this.number = number;  
     }
 }
-    class Add
+    class PersonalList
 {
     public List<Personal> pList;
-    public Add()
+    public PersonalList()
     {
         pList = new List<Personal>();
     }
-    public void AddP()
+    public void Add()
     {
         Console.WriteLine("Vad heter polisen?");
         string name = Console.ReadLine();
@@ -44,24 +44,47 @@ class Utryckning
     public string crime; // typ av brott
     public int time; //vilken tid brottet inträffade.
     public string cops; //Vilka poliser som deltog
-    public string plats; // plats .
-    public List<Utryckning> utryckList;
+    public string place; // plats .
     
-    public Utryckning(string crime, string plats, string cops, int time)
+    
+    public Utryckning(string crime, string place, string cops, int time)
     {
         this.crime = crime;
-        this.plats = plats;
+        this.place = place;
         this.cops = cops;
-        this.time = time;
+        this.time = time; 
+    }
+}
+class UtLista
+{
+    public List<Utryckning> utryckList;
+    public UtLista()
+    {
         utryckList = new List<Utryckning>();
     }
-    public void Add(string crime, string plats, string cops, int time)
+    public void Add()
     {
-        utryckList.Add(new Utryckning(crime, plats, cops, time));
+        Console.WriteLine("Vad var det för brott?");
+        string crime = Console.ReadLine();
+        Console.WriteLine("Var skedde utryckningen?");
+        string place = Console.ReadLine();
+        Console.WriteLine("Vilken tid skedde utryckningen?");
+        int time = int.Parse(Console.ReadLine());
+        Console.WriteLine("Och slutligen vilka poliser var på utryckningen?");
+        string cops = Console.ReadLine();
+        
+        utryckList.Add(new Utryckning(crime, place, cops, time));
     }
-    
-    
+    public void ShowUtlist()
+    {
+        for(int i = 0; i < utryckList.Count; i++)
+        {
+            Console.WriteLine($"Utryckning {i + 1}: {utryckList[i].crime}, {utryckList[i].place}, {utryckList[i].time}, {utryckList[i].time}");
+        }
+    }
 }
+
+
 
         public class Rapport
         {
@@ -95,16 +118,16 @@ class Meny
     public static void Main()
     {
         Rapport rapportInstance = new Rapport(); //för att kunna kalla på metoden
-        Add Add = new Add();
-
-        Console.WriteLine("1. Skriv rapport");
-        Console.WriteLine("2. Lägg till personal");
-        Console.WriteLine("3. Rapporter");
-        Console.WriteLine("4. Exit");
-        string choice = Console.ReadLine();
-
+        PersonalList PersonalList = new PersonalList();
+        UtLista utryck = new UtLista();
         while(true)
         {
+            Console.WriteLine("1. Skriv rapport");
+            Console.WriteLine("2. Lägg till personal");
+            Console.WriteLine("3. Rapporter");
+            Console.WriteLine("4.Arkiv");
+            Console.WriteLine("5. Exit");
+            string choice = Console.ReadLine();
             
             switch(choice)
             {
@@ -112,14 +135,15 @@ class Meny
                     rapportInstance.Rapportering();
                     break;
                 case "2":
-                    Add.AddP();
-                    Add.ShowPeronal();
+                    PersonalList.Add();
                     break;
                 case "3":
-
+                    utryck.Add();
                     break;
                 case "4":
 
+                    break;
+                case "5":
                     return;
             }
         }
